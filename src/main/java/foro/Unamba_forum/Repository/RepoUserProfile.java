922e88f74da34e797caf55bf407dffd66315088b
CristianOlivera1 <221181@unamba.edu.pe>
2025-03-24 20:17:03 -0500

    agregar método para obtener usuarios aleatorios en RepoUserProfile

--- src/main/java/foro/Unamba_forum/Repository/RepoUserProfile.java
+++ src/main/java/foro/Unamba_forum/Repository/RepoUserProfile.java
@@ -2,9 +2,11 @@ package foro.Unamba_forum.Repository;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import foro.Unamba_forum.Entity.TUser;
 import foro.Unamba_forum.Entity.TUserProfile;
@@ -12,4 +14,6 @@ import foro.Unamba_forum.Entity.TUserProfile;
 @Repository
 public interface RepoUserProfile extends JpaRepository<TUserProfile, String> {
     Optional<TUserProfile> findByIdUsuario(TUser idUsuario);
+     @Query(value = "SELECT * FROM perfilusuario ORDER BY RAND() LIMIT ?1", nativeQuery = true)
+    List<TUserProfile> findRandomUsers(int count);
 }
