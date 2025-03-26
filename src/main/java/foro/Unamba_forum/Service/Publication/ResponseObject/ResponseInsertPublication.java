6f1657d5c85fcbe8f8daa1c789bfe4e3815a4bc7
CristianOlivera1 <221181@unamba.edu.pe>
2025-03-25 19:02:57 -0500

    agregar clase ResponseInsertPublication para gestionar la respuesta de inserción de publicaciones

---
+++ src/main/java/foro/Unamba_forum/Service/Publication/ResponseObject/ResponseInsertPublication.java
@@ -0,0 +1,8 @@
+package foro.Unamba_forum.Service.Publication.ResponseObject;
+
+import foro.Unamba_forum.Dto.DtoPublication;
+import foro.Unamba_forum.Service.Generic.ResponseGeneric;
+
+public class ResponseInsertPublication  extends ResponseGeneric<DtoPublication> {
+    
+}
