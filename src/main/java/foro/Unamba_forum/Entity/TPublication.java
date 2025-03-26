1f7c3257efab88e724b1f8947cbaca7278202518
CristianOlivera1 <221181@unamba.edu.pe>
2025-03-25 19:02:17 -0500

    agregar clase TPublication para representar publicaciones con atributos relevantes y relaciones con usuario, categoría y carrera

---
+++ src/main/java/foro/Unamba_forum/Entity/TPublication.java
@@ -0,0 +1,47 @@
+package foro.Unamba_forum.Entity;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "publicacion")
+public class TPublication implements Serializable  {
+      @Id
+    @Column(name = "idPublicacion")
+    private String idPublicacion;
+
+    @ManyToOne
+    @JoinColumn(name = "idUsuario", nullable = false)
+    private TUser usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "idCategoria", nullable = false)
+    private TCategory categoria;
+
+    @ManyToOne
+    @JoinColumn(name = "idCarrera", nullable = false)
+    private TCareer carrera;
+
+    @Column(name = "titulo")
+    private String titulo;
+
+    @Column(name = "contenido")
+    private String contenido;
+
+    @Column(name = "fechaActualizacion")
+    private Timestamp fechaActualizacion;
+
+    @Column(name = "fechaRegistro")
+    private Timestamp fechaRegistro;
+}
