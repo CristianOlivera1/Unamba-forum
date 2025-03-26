f6c88de6877702bfe9426561180f3113c3a0f8c8
CristianOlivera1 <221181@unamba.edu.pe>
2025-03-25 19:01:40 -0500

    agregar clase DtoFile para manejar archivos subidos con atributos relevantes y soporte para MultipartFile

---
+++ src/main/java/foro/Unamba_forum/Dto/DtoFile.java
@@ -0,0 +1,22 @@
+package foro.Unamba_forum.Dto;
+
+import java.sql.Timestamp;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DtoFile {
+    private String idArchivo;
+    private String idPublicacion;
+    private String tipo;
+    private String rutaArchivo;
+    private Timestamp fechaRegistro;
+    @JsonIgnore 
+    private MultipartFile file; // Archivo subido
+}
