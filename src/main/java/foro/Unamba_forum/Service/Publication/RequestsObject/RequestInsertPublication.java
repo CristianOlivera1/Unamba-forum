1ba421f4df3fdaabd7430a5c11ee4809d372bdec
CristianOlivera1 <221181@unamba.edu.pe>
2025-03-25 19:02:47 -0500

    agregar clase RequestInsertPublication para gestionar la inserción de publicaciones con validaciones de campos requeridos

---
+++ src/main/java/foro/Unamba_forum/Service/Publication/RequestsObject/RequestInsertPublication.java
@@ -0,0 +1,30 @@
+package foro.Unamba_forum.Service.Publication.RequestsObject;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RequestInsertPublication {
+    @NotBlank(message = "El campo \"idUsuario \" es requerido")
+    private String idUsuario;
+
+    @NotBlank(message = "El campo \"idCategoria \" es requerido")
+    private String idCategoria;
+
+    @NotBlank(message = "El campo \"idCarrera \" es requerido")
+    private String idCarrera;
+
+    @NotBlank(message = "El campo \"titulo \" es requerido")
+    private String titulo;
+
+    @NotBlank(message = "El campo \"contenido \" es requerido")
+    private String contenido;
+
+    private List<MultipartFile> archivos;
+}
