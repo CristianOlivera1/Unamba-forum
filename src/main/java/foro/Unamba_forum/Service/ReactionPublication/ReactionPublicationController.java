9a8e5f10a197008c4b4ca420fc22b8c598e54d38
CristianOlivera1 <221181@unamba.edu.pe>
2025-03-27 10:44:05 -0500

    agregar controlador ReactionPublication para gestionar reacciones a publicaciones

---
+++ src/main/java/foro/Unamba_forum/Service/ReactionPublication/ReactionPublicationController.java
@@ -0,0 +1,11 @@
+package foro.Unamba_forum.Service.ReactionPublication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequestMapping("/reaction")
+public class ReactionPublicationController {
+    
+}
