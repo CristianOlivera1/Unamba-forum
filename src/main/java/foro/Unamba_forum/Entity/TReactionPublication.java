7d2dac3e6de55e22ddbf0ef15c9d30ec3e09a72c
CristianOlivera1 <221181@unamba.edu.pe>
2025-03-27 15:43:51 -0500

    agregar atributos y relaciones en TReactionPublication para gestionar reacciones a publicaciones

--- src/main/java/foro/Unamba_forum/Entity/TReactionPublication.java
+++ src/main/java/foro/Unamba_forum/Entity/TReactionPublication.java
@@ -1,8 +1,13 @@
 package foro.Unamba_forum.Entity;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,5 +17,21 @@ import lombok.Setter;
 @Entity
 @Table(name = "reaccionpublicacion")
 public class TReactionPublication implements Serializable {
-    
+    @Id
+    @Column(name = "idReaccion")
+    private String idReaccion;
+
+    @ManyToOne
+    @JoinColumn(name = "idUsuario", nullable = false)
+    private TUser usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "idPublicacion", nullable = false)
+    private TPublication publicacion;
+
+    @Column(name = "tipo")
+    private String tipo;
+
+    @Column(name = "fechaReaccion")
+    private Timestamp fechaReaccion;
 }
