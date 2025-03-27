b0e0c1d8d3ae0008f777b54ac25eec37e23163ce
CristianOlivera1 <221181@unamba.edu.pe>
2025-03-27 10:43:46 -0500

    agregar clase TReactionPublication para representar reacciones a publicaciones

---
+++ src/main/java/foro/Unamba_forum/Entity/TReactionPublication.java
@@ -0,0 +1,16 @@
+package foro.Unamba_forum.Entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "reaccionpublicacion")
+public class TReactionPublication implements Serializable {
+    
+}
