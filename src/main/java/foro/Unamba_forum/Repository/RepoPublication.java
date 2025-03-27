d2b4aa9313b85c802ee38dc9525a6e3e9ee933d9
CristianOlivera1 <221181@unamba.edu.pe>
2025-03-27 10:43:50 -0500

    agregar métodos para buscar publicaciones con y sin archivos adjuntos

--- src/main/java/foro/Unamba_forum/Repository/RepoPublication.java
+++ src/main/java/foro/Unamba_forum/Repository/RepoPublication.java
@@ -1,11 +1,18 @@
 package foro.Unamba_forum.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import foro.Unamba_forum.Entity.TPublication;
 
 @Repository
 public interface RepoPublication extends JpaRepository<TPublication, String> {
-    
+  @Query("SELECT p FROM TPublication p WHERE EXISTS (SELECT f FROM TFile f WHERE f.publicacion = p)")
+    Page<TPublication> findPublicationsWithFiles(Pageable pageable);
+
+    @Query("SELECT p FROM TPublication p WHERE NOT EXISTS (SELECT f FROM TFile f WHERE f.publicacion = p)")
+    Page<TPublication> findPublicationsWithoutFiles(Pageable pageable);
+
 }
