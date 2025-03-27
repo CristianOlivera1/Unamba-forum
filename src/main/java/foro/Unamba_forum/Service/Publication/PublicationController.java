5ad54b7724db47c6d97d69f6bed8b1cbf926e6ce
CristianOlivera1 <221181@unamba.edu.pe>
2025-03-27 10:44:01 -0500

    agregar métodos para obtener publicaciones con y sin archivos de forma paginada

--- src/main/java/foro/Unamba_forum/Service/Publication/PublicationController.java
+++ src/main/java/foro/Unamba_forum/Service/Publication/PublicationController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,7 +15,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Page;
 
 import foro.Unamba_forum.Business.BusinessPublication;
 import foro.Unamba_forum.Dto.DtoFile;
@@ -30,12 +34,13 @@ import foro.Unamba_forum.Service.Publication.ResponseObject.ResponseUpdatePublic
 
 @RequestMapping("/publication")
 public class PublicationController {
-    
+
     @Autowired
     private BusinessPublication businessPublication;
 
     @PostMapping("/insert")
-    public ResponseEntity<ResponseInsertPublication> insertPublication(@ModelAttribute RequestInsertPublication request) {
+    public ResponseEntity<ResponseInsertPublication> insertPublication(
+            @ModelAttribute RequestInsertPublication request) {
         ResponseInsertPublication response = new ResponseInsertPublication();
         try {
             DtoPublication dtoPublication = new DtoPublication();
@@ -44,22 +49,22 @@ public class PublicationController {
             dtoPublication.setIdCarrera(request.getIdCarrera());
             dtoPublication.setTitulo(request.getTitulo());
             dtoPublication.setContenido(request.getContenido());
-    
+
             // Procesar archivos solo si se proporcionan
             List<DtoFile> archivos = new ArrayList<>();
             if (request.getArchivos() != null && !request.getArchivos().isEmpty()) {
                 archivos = request.getArchivos().stream().map(file -> {
                     DtoFile dtoArchivo = new DtoFile();
-                    dtoArchivo.setFile(file); // Asignar el archivo al DTO
+                    dtoArchivo.setFile(file);
                     dtoArchivo.setTipo(file.getContentType().startsWith("image") ? "imagen" : "video");
-                    dtoArchivo.setRutaArchivo(file.getOriginalFilename()); // Aquí puedes manejar la subida de archivos
+                    dtoArchivo.setRutaArchivo(file.getOriginalFilename()); 
                     return dtoArchivo;
                 }).collect(Collectors.toList());
             }
             dtoPublication.setArchivos(archivos);
-    
+
             businessPublication.insertPublication(dtoPublication);
-    
+
             response.setType("success");
             response.setListMessage(List.of("Publicación insertada correctamente"));
             response.setData(dtoPublication);
@@ -71,8 +76,26 @@ public class PublicationController {
         }
     }
 
+    //para obtener una publicacion en detalles y precargar para actualizar
+    @GetMapping("/details/{idPublicacion}")
+    public ResponseEntity<ResponseGeneric<DtoPublication>> getPublicationDetails(@PathVariable String idPublicacion) {
+        ResponseGeneric<DtoPublication> response = new ResponseGeneric<>();
+        try {
+            DtoPublication publicationDetails = businessPublication.getPublicationDetails(idPublicacion);
+            response.setType("success");
+            response.setData(publicationDetails);
+            response.setListMessage(List.of("Detalles de la publicación obtenidos correctamente"));
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            response.setType("error");
+            response.setListMessage(List.of("Error al obtener los detalles de la publicación: " + e.getMessage()));
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
     @PutMapping("/update")
-    public ResponseEntity<ResponseUpdatePublication> updatePublication(@ModelAttribute RequestUpdatePublication request) {
+    public ResponseEntity<ResponseUpdatePublication> updatePublication(
+            @ModelAttribute RequestUpdatePublication request) {
         ResponseUpdatePublication response = new ResponseUpdatePublication();
         try {
             DtoPublication dtoPublication = new DtoPublication();
@@ -80,22 +103,22 @@ public class PublicationController {
             dtoPublication.setIdCategoria(request.getIdCategoria());
             dtoPublication.setTitulo(request.getTitulo());
             dtoPublication.setContenido(request.getContenido());
-    
+
             // Procesar archivos solo si se proporcionan
             List<DtoFile> archivos = new ArrayList<>();
             if (request.getArchivos() != null && !request.getArchivos().isEmpty()) {
                 archivos = request.getArchivos().stream().map(file -> {
                     DtoFile dtoArchivo = new DtoFile();
-                    dtoArchivo.setFile(file); // Asignar el archivo al DTO
+                    dtoArchivo.setFile(file); 
                     dtoArchivo.setTipo(file.getContentType().startsWith("image") ? "imagen" : "video");
-                    dtoArchivo.setRutaArchivo(file.getOriginalFilename()); // Aquí puedes manejar la subida del archivo
+                    dtoArchivo.setRutaArchivo(file.getOriginalFilename());
                     return dtoArchivo;
                 }).collect(Collectors.toList());
             }
             dtoPublication.setArchivos(archivos);
-    
+
             businessPublication.updatePublication(dtoPublication);
-    
+
             response.setType("success");
             response.setListMessage(List.of("Publicación actualizada correctamente"));
             response.setData(dtoPublication);
@@ -106,6 +129,43 @@ public class PublicationController {
             return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @GetMapping("/withfiles/paginated")
+    public ResponseEntity<ResponseGetAllPublication> getPublicationsWithFiles(
+            @RequestParam(defaultValue = "0") int page) {
+        ResponseGetAllPublication response = new ResponseGetAllPublication();
+        try {
+            Page<DtoPublication> publications = businessPublication.getPublicationsWithFilesPageable(
+                    PageRequest.of(page, 3, Sort.by(Sort.Direction.DESC, "fechaRegistro")));
+            response.setType("success");
+            response.setData(publications.getContent());
+            response.setListMessage(List.of("Publicaciones con archivos obtenidas correctamente"));
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            response.setType("error");
+            response.setListMessage(List.of("Error al obtener publicaciones con archivos: " + e.getMessage()));
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/withoutfiles/paginated")
+    public ResponseEntity<ResponseGetAllPublication> getPublicationsWithoutFiles(
+            @RequestParam(defaultValue = "0") int page) {
+        ResponseGetAllPublication response = new ResponseGetAllPublication();
+        try {
+            Page<DtoPublication> publications = businessPublication.getPublicationsWithoutFilesPageable(
+                    PageRequest.of(page, 3, Sort.by(Sort.Direction.DESC, "fechaRegistro")));
+            response.setType("success");
+            response.setData(publications.getContent());
+            response.setListMessage(List.of("Publicaciones sin archivos obtenidas correctamente"));
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            response.setType("error");
+            response.setListMessage(List.of("Error al obtener publicaciones sin archivos: " + e.getMessage()));
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
     @DeleteMapping("/delete/{idPublicacion}")
     public ResponseEntity<ResponseGeneric<String>> deletePublication(@PathVariable String idPublicacion) {
         ResponseGeneric<String> response = new ResponseGeneric<>();
@@ -152,4 +212,5 @@ public class PublicationController {
             return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
 }
