588081c9a5df9013549619fd71d03e7f7c7ce9b1
CristianOlivera1 <221181@unamba.edu.pe>
2025-03-27 10:44:15 -0500

    agregar clase ResponseGetAllRP para gestionar respuestas de reacciones a publicaciones

---
+++ src/main/java/foro/Unamba_forum/Service/ReactionPublication/ResponseObject/ResponseGetAllRP.java
@@ -0,0 +1,10 @@
+package foro.Unamba_forum.Service.ReactionPublication.ResponseObject;
+
+import java.util.List;
+
+import foro.Unamba_forum.Dto.DtoReactionPublication;
+import foro.Unamba_forum.Service.Generic.ResponseGeneric;
+
+public class ResponseGetAllRP extends ResponseGeneric<List<DtoReactionPublication>> {
+    
+}
