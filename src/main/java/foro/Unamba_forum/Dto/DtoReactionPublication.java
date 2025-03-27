4cbef5c84a7b9a1e46d25d0bd4838d1ce2548bd8
CristianOlivera1 <221181@unamba.edu.pe>
2025-03-27 15:43:40 -0500

    agregar atributos para gestionar reacciones en DtoReactionPublication

--- src/main/java/foro/Unamba_forum/Dto/DtoReactionPublication.java
+++ src/main/java/foro/Unamba_forum/Dto/DtoReactionPublication.java
@@ -1,10 +1,16 @@
 package foro.Unamba_forum.Dto;
 
+import java.sql.Timestamp;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class DtoReactionPublication {
-    
+    private String idReaccion;
+    private String idUsuario;
+    private String idPublicacion;
+    private String tipo; 
+    private Timestamp fechaReaccion;
 }
