b389a4ad2ccc64fd8e319df92ec25cb6a14957ba
CristianOlivera1 <221181@unamba.edu.pe>
2025-03-28 19:12:01 -0500

    agregar repositorio RepoResponseComment para gestionar respuestas a comentarios

---
+++ src/main/java/foro/Unamba_forum/Repository/RepoResponseComment.java
@@ -0,0 +1,14 @@
+package foro.Unamba_forum.Repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import foro.Unamba_forum.Entity.TResponseComment;
+
+@Repository
+public interface RepoResponseComment extends JpaRepository<TResponseComment, String> {
+    List<TResponseComment> findByComentarioIdComentario(String idComentario);
+    List<TResponseComment> findByRespuestaPadreIdRespuesta(String idRespuestaPadre);
+}
