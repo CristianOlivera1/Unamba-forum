ec95f7fb35117a8e9785e78489039e1a3dfcd94d
CristianOlivera1 <221181@unamba.edu.pe>
2025-03-28 19:12:50 -0500

    agregar clase ResponseGetAllRC para gestionar respuestas de todos los comentarios

---
+++ src/main/java/foro/Unamba_forum/Service/ResponseComment/ResponseObject/ResponseGetAllRC.java
@@ -0,0 +1,10 @@
+package foro.Unamba_forum.Service.ResponseComment.ResponseObject;
+
+import java.util.List;
+
+import foro.Unamba_forum.Dto.DtoResponseComment;
+import foro.Unamba_forum.Service.Generic.ResponseGeneric;
+
+public class ResponseGetAllRC extends ResponseGeneric<List<DtoResponseComment>> {
+    
+}
