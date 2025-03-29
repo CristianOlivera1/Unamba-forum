e0f20cfad7ce96f10311da942771423fbc8d0a5b
CristianOlivera1 <221181@unamba.edu.pe>
2025-03-28 19:11:11 -0500

    agregar DtoResponseComment para gestionar respuestas a comentarios

---
+++ src/main/java/foro/Unamba_forum/Dto/DtoResponseComment.java
@@ -0,0 +1,19 @@
+package foro.Unamba_forum.Dto;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DtoResponseComment {
+    private String idRespuesta;
+    private String idComentario;
+    private String idRespuestaPadre; // NULL si es respuesta directa al comentario y se llena en caso de que sea una respuesta a una respuesta
+    private String idUsuario;
+    private String contenido;
+    private Timestamp fechaRegistro;
+    private List<DtoResponseComment> respuestasHijas; // Respuestas a respuestas
+}
