e32b3ef4b9123c20ac5f1e4ae8c3f62c01d25a52
CristianOlivera1 <221181@unamba.edu.pe>
2025-03-28 19:11:48 -0500

    agregar entidad TReactionComment para gestionar reacciones a comentarios

---
+++ src/main/java/foro/Unamba_forum/Entity/TReactionComment.java
@@ -0,0 +1,41 @@
+package foro.Unamba_forum.Entity;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "reaccioncomentario")
+public class TReactionComment {
+    
+    @Id
+    @Column(name = "idReaccion", nullable = false, length = 36)
+    private String idReaccion;
+
+    @ManyToOne
+    @JoinColumn(name = "idComentario", referencedColumnName = "idComentario", nullable = true)
+    private TCommentPublication comentario;
+
+    @ManyToOne
+    @JoinColumn(name = "idRespuesta", referencedColumnName = "idRespuesta", nullable = true)
+    private TResponseComment respuesta;
+
+    @ManyToOne
+    @JoinColumn(name = "idUsuario", referencedColumnName = "idUsuario", nullable = true)
+    private TUser usuario;
+
+    @Column(name = "tipo")
+    private String tipo;
+
+    @Column(name = "fechaReaccion")
+    private Timestamp fechaReaccion;
+}
