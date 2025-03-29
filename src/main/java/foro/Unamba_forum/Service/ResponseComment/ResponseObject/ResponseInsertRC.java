2c1d039b70745e4afac1e40613081dffeef01338
CristianOlivera1 <221181@unamba.edu.pe>
2025-03-28 19:12:56 -0500

    agregar clase ResponseInsertRC para gestionar respuestas de inserción de comentarios

---
+++ src/main/java/foro/Unamba_forum/Service/ResponseComment/ResponseObject/ResponseInsertRC.java
@@ -0,0 +1,8 @@
+package foro.Unamba_forum.Service.ResponseComment.ResponseObject;
+
+import foro.Unamba_forum.Dto.DtoResponseComment;
+import foro.Unamba_forum.Service.Generic.ResponseGeneric;
+
+public class ResponseInsertRC  extends ResponseGeneric<DtoResponseComment> {
+    
+}
