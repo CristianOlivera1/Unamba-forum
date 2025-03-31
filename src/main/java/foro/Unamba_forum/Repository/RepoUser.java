d383b5e9f54a12bf0c311f9fbb173bb42ac9b9a9
CristianOlivera1 <221181@unamba.edu.pe>
2025-03-31 11:20:34 -0500

    agregar un salto de línea al final del archivo RepoUser.java

--- src/main/java/foro/Unamba_forum/Repository/RepoUser.java
+++ src/main/java/foro/Unamba_forum/Repository/RepoUser.java
@@ -3,6 +3,7 @@ package foro.Unamba_forum.Repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 import foro.Unamba_forum.Entity.TUser;
@@ -12,5 +13,4 @@ public interface RepoUser extends JpaRepository<TUser, String> {
 
      //esto es para validar por independiente los datos en el formulario
    Optional<TUser> findByEmail(String email);
-
 }
