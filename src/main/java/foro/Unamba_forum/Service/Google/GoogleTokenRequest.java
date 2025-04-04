98de7699b3dd36be441cc09808e843efc3e70a70
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-04 17:05:59 -0500

    agregar clase GoogleTokenRequest para gestionar solicitudes de tokens de Google

---
+++ src/main/java/foro/Unamba_forum/Service/Google/GoogleTokenRequest.java
@@ -0,0 +1,11 @@
+
+package foro.Unamba_forum.Service.Google;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GoogleTokenRequest {
+    private String idTokenString;
+    private String accessToken;
+}
