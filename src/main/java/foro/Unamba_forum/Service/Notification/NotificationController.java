5242ceec44188883c03eaed38036571231556613
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-04 17:06:05 -0500

    eliminar importación innecesaria de Page en NotificationController

--- src/main/java/foro/Unamba_forum/Service/Notification/NotificationController.java
+++ src/main/java/foro/Unamba_forum/Service/Notification/NotificationController.java
@@ -3,7 +3,6 @@ package foro.Unamba_forum.Service.Notification;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
