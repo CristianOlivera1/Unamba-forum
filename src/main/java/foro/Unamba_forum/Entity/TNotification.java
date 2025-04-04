a56b478128867dffdeb55e8068116bdae4b5235d
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-04 17:05:41 -0500

    eliminar importación innecesaria de LocalDateTime en TNotification

--- src/main/java/foro/Unamba_forum/Entity/TNotification.java
+++ src/main/java/foro/Unamba_forum/Entity/TNotification.java
@@ -2,7 +2,6 @@ package foro.Unamba_forum.Entity;
 
 import java.io.Serializable;
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
