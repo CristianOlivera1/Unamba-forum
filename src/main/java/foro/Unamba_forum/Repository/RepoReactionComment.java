2a3262594cb582d82bba57a792885e144c076921
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-22 16:37:02 -0500

    agregar métodos para buscar reacciones por usuario en comentarios y respuestas en RepoReactionComment

--- src/main/java/foro/Unamba_forum/Repository/RepoReactionComment.java
+++ src/main/java/foro/Unamba_forum/Repository/RepoReactionComment.java
@@ -1,6 +1,7 @@
 package foro.Unamba_forum.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -25,6 +26,9 @@ public interface RepoReactionComment extends JpaRepository<TReactionComment, Str
 
     List<TReactionComment> findByRespuestaIdRespuestaAndTipo(String idRespuesta, String tipo);
 
-    
-    
+    List<TReactionComment> findByUsuarioIdUsuario(String idUsuario);
+
+    Optional<TReactionComment> findByUsuarioIdUsuarioAndComentarioIdComentario(String idUsuario, String idComentario);
+
+Optional<TReactionComment> findByUsuarioIdUsuarioAndRespuestaIdRespuesta(String idUsuario, String idRespuesta);
 }
