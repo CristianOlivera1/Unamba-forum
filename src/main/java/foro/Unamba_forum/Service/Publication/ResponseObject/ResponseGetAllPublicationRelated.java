538c682ad72ad1ec8eb02248a464962a240d5375
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-29 18:01:37 -0500

    agregar clase ResponseGetAllPublicationRelated para gestionar respuestas de publicaciones relacionadas

---
+++ src/main/java/foro/Unamba_forum/Service/Publication/ResponseObject/ResponseGetAllPublicationRelated.java
@@ -0,0 +1,9 @@
+package foro.Unamba_forum.Service.Publication.ResponseObject;
+
+import java.util.List;
+
+import foro.Unamba_forum.Dto.DtoPublicationRelated;
+import foro.Unamba_forum.Service.Generic.ResponseGeneric;
+
+public class ResponseGetAllPublicationRelated extends ResponseGeneric<List<DtoPublicationRelated>> {
+}
