700074a50eb45b1f7609cfda7b60b0350ac8789f
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-29 18:00:58 -0500

    agregar la ruta de categoría a las solicitudes permitidas en la configuración de seguridad

--- src/main/java/foro/Unamba_forum/SecurityConfig.java
+++ src/main/java/foro/Unamba_forum/SecurityConfig.java
@@ -12,7 +12,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.cors(cors -> cors.disable()).csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/oauth2/callback/**","/career/**","/user/**","/userprofile/**","/totals/**","/publication/**","/reaction/**","/commentpublication/**","/follow/**","/rol/**","/reactioncomment/**","/responsecomment/**").permitAll()
+                        .requestMatchers("/oauth2/callback/**","/career/**","/user/**","/userprofile/**","/totals/**","/publication/**","/reaction/**","/commentpublication/**","/follow/**","/rol/**","/reactioncomment/**","/responsecomment/**","/category/**").permitAll()
                         .anyRequest().authenticated());
         return http.build();
     }
